62e9197924e5b6e64803ee637ab1d92c4f24480c
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2022-02-13 23:12:17 -0300

    implementation project download background

--- AppleMusic/AppDelegate.swift
+++ AppleMusic/AppDelegate.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var backgroundSessionCompletionHandler: (() -> Void)?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +31,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        backgroundSessionCompletionHandler = completionHandler
+        let configuration = URLSessionConfiguration.background(withIdentifier: identifier)
+        let downloadSession = URLSession(configuration: configuration, delegate: MusicViewController(), delegateQueue: nil)
+        let downloadService = DownloadService()
+        downloadService.downloadsSession = downloadSession
+    }
 
 }
 
